58bb2925ac866255e3567fcc3a5c1a0398d63b22
Hamilton Paz <73714770+HamPaz@users.noreply.github.com>
2021-03-12 09:51:26 -0300

    Acréscimo de palavras
    Acréscimo de palavras para tornar o jogo mais desafiante.

--- palavras.py
+++ palavras.py
@@ -1,6 +1,8 @@
 categoria_palavra = {
     'herois'   : ['superman', 'mulher-maravilha', 'batman', 'aquaman', 'lanterna-verde',
     'flash', 'batgirl', 'canario-negro', 'arqueiro-verde', 'supergirl'],
+    
     'medieval' : ['armadura', 'espada', 'elmo', 'escudo', 'castelo', 'cavaleiro', 'princesa',
-    'taverna']
+    'taverna', 'escudeiro', 'arqueiro', 'bardo', 'rei', 'rainha', 'mago', 'feiticeiro']
 }
+
